aef77272b4dbf959ef3001dadac0799dc0197b31
xiejj <thankdj@yeah.net>
2019-12-16 18:01:36 +0800

    文章详情查看

--- cuttlefish-content-api/src/main/java/com/thank/cuttlefish/content/api/ContentApi.java
+++ cuttlefish-content-api/src/main/java/com/thank/cuttlefish/content/api/ContentApi.java
@@ -6,12 +6,13 @@ import com.thank.cuttlefish.content.fallback.ContentApiFallback;
 import com.thank.cuttlefish.content.pojo.Content;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @FeignClient(value = "cuttlefish-content", path = "content", fallback = ContentApiFallback.class)
 public interface ContentApi extends Api<Content> {
     @PostMapping(value = "/queryListByRand")
-    List<ContentDto> queryListByRand(ContentDto contentDto);
+    List<ContentDto> queryListByRand(@RequestBody ContentDto contentDto);
 
 }
--- cuttlefish-content/src/main/java/com/thank/cuttlefish/content/service/impl/ContentServiceImpl.java
+++ cuttlefish-content/src/main/java/com/thank/cuttlefish/content/service/impl/ContentServiceImpl.java
@@ -8,7 +8,6 @@ import com.thank.cuttlefish.content.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 
